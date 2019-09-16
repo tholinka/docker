0c92f86e9d1f57ce6f9de2c71cae8bdf9b0dda77
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2019-09-16 11:51:39 -0600

    Add runeliteplus

---
+++ runeliteplus/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+version="2.1.5.0-2"
+tag="runeliteplus:$version"
+
+# build - should generally be all cached so it's quick
+docker build -f Dockerfile -t $tag --build-arg version=$version .
+
+# setup X11
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+# assume we've already done the xauth call if $XAUTH has stuff in it
+if ! [ -s $XAUTH ]; then
+	touch $XAUTH
+	xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+fi
+
+x11args="-v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH"
+volumes="-v $(pwd)/volumes/runelite:/root/.runelite"
+
+# run
+docker run -it $x11args $volumes $tag $@
